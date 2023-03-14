92c9b855f8ea2ea9abfd95e90824c7b8ac3321d5
bielas <kamilbielawski13@gmail.com>
2023-03-14 18:56:17 +0100

    update 14.03

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactCurvedText from "react-curved-text";
 
 export default function Header() {
     return (
         <div className="header">
             <h1>
-                Header
+                <span className="char1">K</span>
+                <span className="char1">B</span>
             </h1>
         </div>
     )
