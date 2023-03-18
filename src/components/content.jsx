7833ea830e5ba752d0a4ad2876d97c2ac1e7718c
bielas <kamilbielawski13@gmail.com>
2023-03-18 11:34:26 +0100

    18.03

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -7,8 +7,7 @@ export default function Content() {
                 <p>Hi, my name is</p>
                 <h1>Kamil Bielawski.</h1>
                 <h3>Front-end developer</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquam sit ipsam repellat harum labore eum, sunt quasi dicta recusandae. Quisquam, ullam explicabo vel aliquam adipisci cupiditate quia minus iste.
-                Exercitationem amet, quam quaerat in, tenetur possimus molestias earum blanditiis repudiandae soluta beatae nobis? Ratione porro maxime incidunt ipsam deleniti sint at soluta omnis, illo ea officia amet et? Dicta.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquam sit ipsam repellat harum labore eum, sunt quasi dicta recusandae. Quisquam, ullam explicabo vel aliquam adipisci cupiditate quia minus iste.</p>
             </div>
             <div className="content-right">
                 <h1>Image</h1>
