7865323f90aa732d19449ee6dba162d897a8c30c
bielas <kamilbielawski13@gmail.com>
2023-03-27 16:09:29 +0200

    update 27.03

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function About() {
     return (
         <div className="about">
-            
+            <h2>About</h2>
+            <p>Hi there! My name is <span>Kamil Bielawski</span>, and I'm a third-year student studying front-end web development in Gdańsk. I'm currently learning and using React in my projects, and it's quickly become my favorite library to work with. I love how it simplifies the process of building complex user interfaces and makes it easy to reuse code across different parts of a website.</p>
+            <p>As a front-end developer, I'm passionate about creating beautiful, responsive websites that provide a great user experience. During my studies, I've had the opportunity to work on a variety of projects, both individually and as part of a team. I've learned how to code using HTML, CSS, and JavaScript, and I'm constantly looking for new ways to improve my skills and stay up-to-date with the latest industry trends. </p>
+            <p>I'm excited to continue growing as a developer and to share my skills with the world. If you'd like to get in touch, please don't hesitate to reach out!</p>
         </div>
     )
 }
