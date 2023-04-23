a15fe6295e93c0d9224395c8e1e7095b96f9b389
bielas <kamilbielawski13@gmail.com>
2023-04-24 00:17:17 +0200

    update 24.04

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import github from '../images/github.png'
 
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    
     return (
         <div className="footer">
+            <div className="footer-text">
+                <span>© Copyright {currentYear} - Kamil Bielawski</span>
+            </div>
             <div className="icons">
                 <a href="https://github.com/biel4s" target="_blank" rel="noreferrer">
                     <img src={github} alt="github" className="link" id="github"/>
@@ -15,6 +20,7 @@ export default function Footer() {
                     <img src={github} alt="github" className="link" id="github"/>
                 </a>
             </div>
+            
         </div>
     )
 }
