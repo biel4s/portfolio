8fa7eac1eb241db270c703b8cfe9f7a6422a943f
bielas <kamilbielawski13@gmail.com>
2023-04-24 16:10:01 +0200

    update 24.04

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,19 +8,26 @@ export default function Footer() {
     
     return (
         <div className="footer">
-            <div className="footer-text">
-                <span>© Copyright {currentYear} - Kamil Bielawski</span>
+            <div className="footer-top">
+                <h4>ABOUT</h4>
+                <h4>PROJECTS</h4>
+                <h4>CONTACT</h4>
             </div>
-            <div className="icons">
-                <a href="https://github.com/biel4s" target="_blank" rel="noreferrer">
-                    <img src={github} alt="github" className="link" id="github"/>
-                </a>
-                <a href="https://www.linkedin.com/in/kamil-bielawski" target="_blank" rel="noreferrer">
-                    <img src={linkedin} alt="linkedin" className="link" id="linkedin"/>
-                </a>
-                <a href="https://www.facebook.com/biel4s" target="_blank" rel="noreferrer">
-                    <img src={facebook} alt="facebook" className="link" id="facebook"/>
-                </a>
+            <div className="footer-bottom">
+                <div className="footer-text">
+                    <span>© Copyright {currentYear} - Kamil Bielawski</span>
+                </div>
+                <div className="footer-icons">
+                    <a href="https://github.com/biel4s" target="_blank" rel="noreferrer">
+                        <img src={github} alt="github" className="link" id="github"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/kamil-bielawski" target="_blank" rel="noreferrer">
+                        <img src={linkedin} alt="linkedin" className="link" id="linkedin"/>
+                    </a>
+                    <a href="https://www.facebook.com/biel4s" target="_blank" rel="noreferrer">
+                        <img src={facebook} alt="facebook" className="link" id="facebook"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
