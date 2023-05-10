c3c2b513a881706219cee0b3ea6cb21ccceb16b7
bielas <kamilbielawski13@gmail.com>
2023-05-11 00:20:42 +0200

    update 11.05

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../assets/images/logo-white.png";
 import style from "./header.module.scss";
 
+// function useScrollDirection() {
+// 	const [isHidden, setIsHidden] = useState(null);
+
+// 	useEffect(() => {
+// 		let lastScrollY = window.pageYOffset;
+
+// 		const updateScrollDirection = () => {
+// 			const scrollY = window.pageYOffset;
+// 			const direction = scrollY > lastScrollY ? "down" : "up";
+
+// 			if (
+// 				direction !== isHidden &&
+// 				Math.abs(
+// 					scrollY - lastScrollY > 1 || scrollY - lastScrollY < -1
+// 				)
+// 			) {
+// 				setIsHidden(direction);
+// 			}
+// 			lastScrollY = scrollY > 0 ? scrollY : 0;
+// 		};
+
+// 		window.addEventListener("scroll", updateScrollDirection);
+// 		return () => {
+// 			window.removeEventListener("scroll", updateScrollDirection);
+// 		}
+// 	}, [isHidden]);
+// 	return isHidden;
+// };
+
+function useScrollDirection() {
+	const [lastScrollY, setLastScrollY] = useState(0);
+	const [isHidden, setIsHidden] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollY = window.pageYOffset;
+			const direction = scrollY > lastScrollY ? "down" : "up";
+			if (direction !== isHidden && Math.abs(scrollY - lastScrollY) > 1) {
+				setIsHidden(direction === "down");
+			}
+			setLastScrollY(scrollY);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [lastScrollY, isHidden]);
+
+	return isHidden ? "hide" : "show";
+}
+
 export default function Header() {
+	const [background, setBackground] = useState(false);
 
 	const navLinks = ["HOME", "ABOUT", "PROJECTS", "CONTACT"];
 
@@ -13,11 +64,30 @@ export default function Header() {
 					<button className={style.button}>{content}</button>
 				</li>
 			</ul>
-		)
-	}
+		);
+	};
+
+	// const isHidden = (useScrollDirection() === "down" ? "hide" : "show");
+
+	const changeBackground = () => {
+		if (window.scrollY >= 50) {
+			setBackground(true);
+		} else {
+			setBackground(false);
+		}
+	};
+	window.addEventListener("scroll", changeBackground);
+
+	const isHidden = useScrollDirection();
 
 	return (
-		<header className={style.container}>
+		<header
+			className={`
+				${style.container} 
+				${style[isHidden]} 
+				${style[background ? "active" : ""]}
+			`}
+		>
 			<div className={style.left}>
 				<img
 					src={logo}
@@ -27,8 +97,8 @@ export default function Header() {
 					alt="logo"
 				/>
 				<h2 className={style.headingPrimary}>
-					KAMIL
-					<span className={style.highlight}>&nbsp;BIELAWSKI</span>
+					kamil
+					<span className={style.highlight}>&nbsp;bielawski</span>
 				</h2>
 			</div>
 			<nav className={style.right}>
