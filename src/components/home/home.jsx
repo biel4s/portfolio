ba44190fab1f3cc2faf30fca8a92295d00597ac1
bielas <kamilbielawski13@gmail.com>
2023-05-30 15:01:18 +0200

    add onClick function to home button

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,21 +4,27 @@ import { mdiArrowRight } from "@mdi/js";
 import style from "./home.module.scss";
 
 export default function Home(props) {
+	const scrollToProject = "projects_container";
+
+	const handleClick = () => {
+		document
+			.getElementById(scrollToProject)
+			.scrollIntoView({ behavior: "smooth" });
+	};
+
 	return (
 		<div className={style.container} id="home_container">
 			<div className={style.content}>
 				<p className={style.text}>Hello, my name is</p>
 				<h1 className={style.headingPrimary}>Kamil Bielawski</h1>
-				<h3 className={style.headingSecondary}>
-					Front-end developer
-				</h3>
+				<h3 className={style.headingSecondary}>Front-end developer</h3>
 				<p className={style.description}>
 					With a passion for creating beautiful, responsive websites
 					and a dedication to learning the latest front-end
 					technologies
 				</p>
 			</div>
-			<button className={style.btn}>
+			<button onClick={handleClick} className={style.btn}>
 				View my projects
 				<Icon className={style.arrow} path={mdiArrowRight} size={1} />
 			</button>
